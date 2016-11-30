942e3b42eace6b132d0f0a26c28dd84b1fb5c36a
AD\imai-yuto <imai-yuto@imjp.co.jp>
2016-11-30 16:44:08 +0900

    最新記事、あきらめました

--- wp-content/themes/newshub/framework/modules/blog/templates/lists/parts/section-title.php
+++ wp-content/themes/newshub/framework/modules/blog/templates/lists/parts/section-title.php
@@ -1,9 +1,10 @@
-            $category_title = $tax->name;
-            $title          = $category_title;
-              
-       <div class="mkd-layout-title-holder"><div class="mkd-section-title-holder clearfix">
+<?php 
+	$cat = get_the_category();
+	$cat = $cat[0];
+?>
+                <div class="mkd-layout-title-holder"><div class="mkd-section-title-holder clearfix">
         <h5 class="mkd-title-line-head">
-            <?php echo esc_html('get_the_category();の最新記事'); ?>
+            最新記事
         </h5>
         <div class="mkd-title-line-body"></div>
     </div>
--- wp-content/themes/newshub/framework/modules/blog/templates/parts/post-info/post-info-author.php
+++ wp-content/themes/newshub/framework/modules/blog/templates/parts/post-info/post-info-author.php
@@ -1,1 +1,1 @@
-<div class="mkd-post-info-author"><a itemprop="author" class="mkd-post-info-author-link" href="<?php echo esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )); ?>"><?php esc_html_e('by: ','newshub').the_author_meta('display_name'); ?></a></div>
+<div class="mkd-post-info-author"><a itemprop="author" class="mkd-post-info-author-link" href="<?php echo esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )); ?>"><?php esc_html_e('投稿者: ','newshub').the_author_meta('display_name'); ?></a></div>
