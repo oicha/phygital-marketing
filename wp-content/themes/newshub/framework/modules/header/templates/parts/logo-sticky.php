ea05786e1318de7539e02c4a06e5f72e0bcb4fed
t-izumi <t-izumi@imjp.co.jp>
2016-11-30 16:07:49 +0900

    ロゴの隣にサイト案内テキスト追加

--- wp-content/themes/newshub/framework/modules/header/templates/parts/logo-sticky.php
+++ wp-content/themes/newshub/framework/modules/header/templates/parts/logo-sticky.php
@@ -1,9 +1,11 @@
 <?php do_action('newshub_mikado_before_site_logo'); ?>
 
-<div class="mkd-logo-wrapper">
+<div class="mkd-logo-wrapper" style="float:left;">
     <a href="<?php echo esc_url(home_url('/')); ?>" <?php newshub_mikado_inline_style($logo_styles); ?>>
         <img src="<?php echo esc_url($logo_image); ?>" alt="<?php esc_html_e('logo','newshub' ); ?>"/>
     </a>
 </div>
+<div style="width:500px; float:right;padding-top:15px;">フィジカルとデジタルをかけ合わせた造語であるPhygital（フィジタル）。
+主に小売業のマーケティングで活用が広がるフィジタル関連情報を配信中。</div>
 
 <?php do_action('newshub_mikado_after_site_logo'); ?>
