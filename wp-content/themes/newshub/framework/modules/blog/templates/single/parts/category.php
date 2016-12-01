f1c02dcfb4be18b270e4a73eeae28252a0ec3904
AD\imai-yuto <imai-yuto@imjp.co.jp>
2016-12-01 18:03:28 +0900

    no message

--- wp-content/themes/newshub/framework/modules/blog/templates/single/parts/category.php
+++ wp-content/themes/newshub/framework/modules/blog/templates/single/parts/category.php
@@ -1,10 +1,7 @@
 <?php if ($display_category == 'yes') { ?>
     <div class="mkd-post-info-category-holder clearfix">
-        <h3 style="
-    margin-top: 0px;
-    margin-bottom: 0px;
-"><div class="waku"><?php newshub_mikado_post_info(array(
+<div class="waku"><?php newshub_mikado_post_info(array(
             'category' => $display_category,
-        )) ?></a></div>
+        )) ?></div>
     </div>
 <?php } ?>
